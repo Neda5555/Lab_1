ce2383534a68c0d35a7e826718d4a26af103da9d
ViliusKumparskas <86661726+ViliusKumparskas@users.noreply.github.com>
2023-10-03 00:55:21 +0300

    Pakeistas button ir text view

--- app/src/main/java/com/example/tesatas5/MainActivity.java
+++ app/src/main/java/com/example/tesatas5/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.tesatas5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewmain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.textViewmain = findViewById(R.id.textViewmain);
+    }
+
+    public void bottonchangeclick(View view) {
+        this.textViewmain.setText("Labas");
     }
 }
